d35a6aa30ff97fc70106dfb04cc5559508bcaec3
leogjhuang <77544794+leogjhuang@users.noreply.github.com>
2021-11-28 20:52:25 -0500

    Revert commit

---
+++ SE101 F21/Shared/SE101_F21App.swift
@@ -0,0 +1,17 @@
+//
+//  SE101_F21App.swift
+//  Shared
+//
+//  Created by Ryan Chen on 2021-11-23.
+//
+
+import SwiftUI
+
+@main
+struct SE101_F21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
